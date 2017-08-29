c19935a8827c7935e42703d21af32325df766247
sravanasandhya <sandhya.kasam@mtuity.com>
2017-08-29 11:17:56 +0530

    javascript samples

---
+++ alert.js
@@ -0,0 +1,40 @@
+
+var name = 'John';
+
+var admin;
+
+var venus;
+
+admin = name;
+
+var name1 = true;
+
+alert(typeof name1);
+
+name1 = String(name1);
+
+alert(typeof name1);
+var name = prompt("what is your name");
+
+alert(name);
+
+if("0")
+{
+    alert("Hello");
+}
+
+var isBoss = confirm("Are you the boss?");
+
+alert( isBoss );
+
+function makeUser(name,age)
+{
+    return{
+      name : name,
+        age: age
+    };
+
+}
+
+
+
